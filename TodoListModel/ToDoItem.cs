9b27eb169405e656910f83beb339221b0db4e610
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-02-13 19:57:42 +0000

    ToDoItem class was registered to the DataContext

--- TodoListModel/ToDoItem.cs
+++ TodoListModel/ToDoItem.cs
@@ -1,9 +1,45 @@
 ﻿namespace TodoListModel;
 
-public class ToDoItem
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class ToDoItem : DbContext
 {
-    public int TodoId { get; set; }
-    public string TaskName { get; set; }
+    public DbSet<Blog> TodoId { get; set; }
+    public DbSet<Post> TaskName { get; set; }
     public bool IsCompleted { get; set; }
 
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
 }
